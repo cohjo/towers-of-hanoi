fb20da193234cdf8ca671a8470bb205795a4851d
conorjohnston <cnrswim@yahoo.com>
2019-10-22 15:19:07 -0400

    Added logic

--- script.js
+++ script.js
@@ -43,13 +43,37 @@ var selected = [];
     // }
 
 firstBox.addEventListener('click', function(e) {
-            e.preventDefault();
-            
-            if (selected.length === 0) {
-                selected.push(discsA[0]);
-                firstBox.removeChild(discsA[0]);
-            } else {
-                firstBox.insertBefore(selected[0], discsA[0]);
-                selected.shift();
-            }
-        })
+    e.preventDefault();
+    
+    if (selected.length == 0) {
+        selected.push(discsA[0]);
+        firstBox.removeChild(discsA[0]);
+    } else {
+        firstBox.insertBefore(selected[0], discsA[0]);
+        selected.shift();
+    }
+})
+
+secondBox.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    if (selected.length == 0) {
+        selected.push(discsB[0]);
+        secondBox.removeChild(discsB[0]);
+    } else {
+        secondBox.insertBefore(selected[0], discsB[0]);
+        selected.shift();
+    }
+})
+
+thirdBox.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    if (selected.length === 0) {
+        selected.push(discsC[0]);
+        thirdBox.removeChild(discsC[0]);
+    } else {
+        thirdBox.insertBefore(selected[0], discsC[0]);
+        selected.shift();
+    }
+})
