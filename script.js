3f090abb6dea86591c1f8e16c66458fa5221f243
conorjohnston <cnrswim@yahoo.com>
2019-10-24 14:55:57 -0400

    Added styling

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const score2 = document.getElementById('disc2').getAttribute('score');
 const score3 = document.getElementById('disc3').getAttribute('score');
 const score4 = document.getElementById('disc4').getAttribute('score');
 
+const hold = document.getElementById('holder');
+
 var selected = [];
 var moves = 0;
 
@@ -27,16 +29,21 @@ firstBox.addEventListener('click', function(e) {
     if(selected.length !== 0 || discsA.length !== 0) {
         if (selected.length == 0) {
             selected.push(discsA[0]);
-            firstBox.removeChild(discsA[0]);
+            hold.appendChild(discsA[0]);
+            var curr = hold.children[0];
+            curr.style.height = '90%';
         } else {
             if (discsA.length === 0) {
                 firstBox.insertBefore(selected[0], discsA[0]);
+                discsA[0].style.height = '15%'
                 selected.shift();
 
                 moves += 1;
                 tracker.textContent = moves;
             } else if (discsA[0].getAttribute('score') < selected[0].getAttribute('score')) {
                 firstBox.insertBefore(selected[0], discsA[0]);
+                discsA[0].style.height = '15%'
+
                 selected.shift();
 
                 moves += 1;
@@ -54,16 +61,21 @@ secondBox.addEventListener('click', function(e) {
     if(selected.length !== 0 || discsB.length !== 0) {
         if (selected.length == 0) {
             selected.push(discsB[0]);
-            secondBox.removeChild(discsB[0]);
+            hold.appendChild(discsB[0]);
+            // secondBox.removeChild(discsB[0]);
+            var curr = hold.children[0];
+            curr.style.height = '90%';
         } else {
             if (discsB.length === 0) {
                 secondBox.insertBefore(selected[0], discsB[0]);
+                discsB[0].style.height = '15%'
                 selected.shift();
 
                 moves += 1;
                 tracker.textContent = moves;
             } else if (discsB[0].getAttribute('score') < selected[0].getAttribute('score')) {
                 secondBox.insertBefore(selected[0], discsB[0]);
+                discsB[0].style.height = '15%'
                 selected.shift();
 
                 moves += 1;
@@ -81,16 +93,21 @@ thirdBox.addEventListener('click', function(e) {
     if(selected.length !== 0 || discsC.length !== 0) {
         if (selected.length === 0) {
             selected.push(discsC[0]);
-            thirdBox.removeChild(discsC[0]);
+            hold.appendChild(discsC[0]);
+            // thirdBox.removeChild(discsC[0]);
+            var curr = hold.children[0];
+            curr.style.height = '90%';
         } else {
             if (discsC.length === 0) {
                 thirdBox.insertBefore(selected[0], discsC[0]);
+                discsC[0].style.height = '15%'
                 selected.shift();
 
                 moves += 1;
                 tracker.textContent = moves;
             } else if (discsC[0].getAttribute('score') < selected[0].getAttribute('score')) {
                 thirdBox.insertBefore(selected[0], discsC[0]);
+                discsC[0].style.height = '15%'
                 selected.shift();
 
                 moves += 1;
